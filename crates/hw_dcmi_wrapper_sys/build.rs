d47dfd834acd8a16b29e5ed15133f12d4acc84e9
Zhu Zijun <2961163526@qq.com>
2025-02-07 14:14:34 +0800

    fix(build): fix link error when `HW_BINDING_BUILD` is false

--- crates/hw_dcmi_wrapper_sys/build.rs
+++ crates/hw_dcmi_wrapper_sys/build.rs
@@ -16,23 +16,23 @@ fn init_bindgen_builder(header: impl Into<String>) -> bindgen::Builder {
 }
 
 fn main() {
-    // 当且仅当HW_DCMI_BUILD为true时才生成绑定
-    println!("cargo:rerun-if-env-changed=HW_DCMI_BUILD");
-    if env::var("HW_DCMI_BUILD").is_err() {
-        return;
-    }
-    if env::var("HW_DCMI_BUILD").unwrap() != "true" {
-        return;
-    }
-
     // 读取环境变量HW_DCMI_PATH作为库搜索路径
     let hw_dcmi_path = env::var("HW_DCMI_PATH").unwrap_or_else(|_| "/usr/local/dcmi".to_string());
-    let interface_path = format!("{}/dcmi_interface_api.h", hw_dcmi_path);
     println!("cargo:rustc-link-search=native={}", hw_dcmi_path);
 
-    // Tell cargo to tell rustc to link the dcmi shared library.
     #[cfg(not(feature = "load_dynamic"))]
     println!("cargo:rustc-link-lib=dylib=dcmi");
+
+    // 当且仅当HW_DCMI_BINDING_BUILD为true时才生成绑定
+    println!("cargo:rerun-if-env-changed=HW_DCMI_BINDING_BUILD");
+    if env::var("HW_DCMI_BINDING_BUILD").is_err() {
+        return;
+    }
+    if env::var("HW_DCMI_BINDING_BUILD").unwrap() != "true" {
+        return;
+    }
+
+    let interface_path = format!("{}/dcmi_interface_api.h", hw_dcmi_path);
 
     // The bindgen::Builder is the main entry point to bindgen,
     // and lets you build up options for the resulting bindings.
