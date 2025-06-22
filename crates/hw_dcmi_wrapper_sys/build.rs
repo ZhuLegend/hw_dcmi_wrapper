4fe5251fba776b57e06fdd86e2516eb53e0c0422
Zhu Zijun <2961163526@qq.com>
2025-06-22 19:35:02 +0800

    feat(bindings)!: add optional bindgen feature for regenerating bindings
    Update build script to conditionally generate bindings only when the `bindgen` feature is enabled and `HW_DCMI_BINDING_BUILD` is set to true.
    Make bindgen an optional dependency and add corresponding feature flags in Cargo.toml.
    Clarify README instructions to require enabling the `bindgen` feature for binding.
    
    BREAKING CHANGE: add optional bindgen feature for regenerating bindings, not require bindgen as deps for most users

--- crates/hw_dcmi_wrapper_sys/build.rs
+++ crates/hw_dcmi_wrapper_sys/build.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::path::PathBuf;
 
+#[cfg(feature = "bindgen")]
 fn init_bindgen_builder(header: impl Into<String>) -> bindgen::Builder {
     bindgen::Builder::default()
         // The input header we would like to generate bindings for.
@@ -24,42 +24,46 @@ fn main() {
     #[cfg(not(feature = "load_dynamic"))]
     println!("cargo:rustc-link-lib=dylib=dcmi");
 
-    // 当且仅当HW_DCMI_BINDING_BUILD为true时才生成绑定
-    println!("cargo:rerun-if-env-changed=HW_DCMI_BINDING_BUILD");
-    if env::var("HW_DCMI_BINDING_BUILD").is_err() {
-        return;
+    #[cfg(feature = "bindgen")]
+    {
+        // 当且仅当HW_DCMI_BINDING_BUILD为true时才生成绑定
+        println!("cargo:rerun-if-env-changed=HW_DCMI_BINDING_BUILD");
+        if env::var("HW_DCMI_BINDING_BUILD").is_err() {
+            return;
+        }
+        if env::var("HW_DCMI_BINDING_BUILD").unwrap() != "true" {
+            return;
+        }
+
+        let interface_path = format!("{}/dcmi_interface_api.h", hw_dcmi_path);
+
+        // The bindgen::Builder is the main entry point to bindgen,
+        // and lets you build up options for the resulting bindings.
+
+        let builder = init_bindgen_builder(interface_path);
+
+        #[cfg(feature = "load_dynamic")]
+        let builder = builder
+            .dynamic_library_name("dcmi")
+            .dynamic_link_require_all(true);
+
+        let bindings = builder
+            // Finish the builder and generate the bindings.
+            .generate()
+            // Unwrap the Result and panic on failure.
+            .expect("Unable to generate bindings");
+
+        // 指定输出文件的路径为 src/hw_dcmi_wrapper_sys.rs
+        #[cfg(feature = "load_dynamic")]
+        let out_path =
+            PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/bindings_dyn.rs");
+        #[cfg(not(feature = "load_dynamic"))]
+        let out_path =
+            PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/bindings.rs");
+
+        // Write the bindings to the specified file.
+        bindings
+            .write_to_file(out_path)
+            .expect("Couldn't write bindings!");
     }
-    if env::var("HW_DCMI_BINDING_BUILD").unwrap() != "true" {
-        return;
-    }
-
-    let interface_path = format!("{}/dcmi_interface_api.h", hw_dcmi_path);
-
-    // The bindgen::Builder is the main entry point to bindgen,
-    // and lets you build up options for the resulting bindings.
-
-    let builder = init_bindgen_builder(interface_path);
-
-    #[cfg(feature = "load_dynamic")]
-    let builder = builder
-        .dynamic_library_name("dcmi")
-        .dynamic_link_require_all(true);
-
-    let bindings = builder
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // 指定输出文件的路径为 src/hw_dcmi_wrapper_sys.rs
-    #[cfg(feature = "load_dynamic")]
-    let out_path =
-        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/bindings_dyn.rs");
-    #[cfg(not(feature = "load_dynamic"))]
-    let out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/bindings.rs");
-
-    // Write the bindings to the specified file.
-    bindings
-        .write_to_file(out_path)
-        .expect("Couldn't write bindings!");
 }
